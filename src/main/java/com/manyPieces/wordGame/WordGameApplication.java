70b7f7b73a4059cda1735f8ff6a158f0c89c3424
Leo <yggb159@naver.com>
2023-08-12 10:50:11 +0900

    initial commit

---
+++ src/main/java/com/manyPieces/wordGame/WordGameApplication.java
@@ -0,0 +1,13 @@
+package com.manyPieces.wordGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordGameApplication.class, args);
+	}
+
+}
