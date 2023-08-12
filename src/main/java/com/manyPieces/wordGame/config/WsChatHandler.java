73288f606604e72fbcb775364e6b8d04d8a2685c
Leo <yggb159@naver.com>
2023-08-12 18:56:31 +0900

    spring websocket

---
+++ src/main/java/com/manyPieces/wordGame/config/WsChatHandler.java
@@ -0,0 +1,32 @@
+package com.manyPieces.wordGame.config;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.manyPieces.wordGame.chat.ChatMessage;
+import com.manyPieces.wordGame.chat.ChatRoom;
+import com.manyPieces.wordGame.chat.ChatService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class WsChatHandler extends TextWebSocketHandler {
+
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("payload {}", payload);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+        ChatRoom room = chatService.findRoomById(chatMessage.getRoomId());
+        room.handleActions(session, chatMessage, chatService);
+
+    }
+}
